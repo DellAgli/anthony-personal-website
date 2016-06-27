dda4438ae68ae85bc1368782a22a94b652d593a8
DellAgli <anthony.dellagli@gmail.com>
2016-06-27 13:59:46 -0400

    Projects cards generated via JS

---
+++ js/projects.js
@@ -0,0 +1,30 @@
+PROJECTS = [
+{
+	title: "TwitchWall",
+	desciption: "TwitchWall is a companion site for the videogame livestreaming site Twitch.tv. It allows the user to easily watch multiple livestreams all on the same page. Unlike other sites, it has the ability to automatically fill out the page with livestreams based on how many other people are currently watching.",
+	imgLink: "media/TwitchWall.png",
+	hostLink: "http://twitchwall.dellag.li",
+	githubLink: "https://github.com/DellAgli/TwitchWall"
+
+},
+
+{
+	title: "Quizzical",
+	desciption: "Quizzical is a social quiz and survey game. It provides a platform for you and your friends to come together, answer questions about themselves, and finally test your friendship by taking a quiz constructed from your friend's answers.",
+	imgLink: "media/Quizzical.png",
+	hostLink: null,
+	githubLink: "https://github.com/DellAgli/Quizzical-Social-Quiz-Game"
+
+},
+
+/**
+{
+	title: "",
+	desciption: "",
+	imgLink: "",
+	hostLink: "",
+	githubLink: ""
+
+},
+*/	
+]
